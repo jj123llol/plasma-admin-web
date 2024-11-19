139e3a0e6aaeb65aa45a07ef1465b6d62934a048
JJJ <115508192+jj123llol@users.noreply.github.com>
2024-11-18 19:56:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function addFunctionality(button,link, isSelf){
 addFunctionality("cmds", "/plasma-admin-web/commands", true) // Open The Command Thing OFC
 
 addFunctionality("ds", "https://discord.gg/XPgzdFZtYw", false) // Open Discord
+
+document.getElementById("Loadstring").onclick = function() {
+    navigator.clipboard.writeText(document.getElementById("Loadstring").textContent);
+}
